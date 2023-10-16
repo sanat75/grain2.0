be4391d2989d312edfa76e9cde13b0111841d742
Harsh Kumar Raghav <raghavharsh068@gmail.com>
2023-10-17 02:54:47 +0530

    Scrollable Brand UI done

--- src/components/Landing/Brands.tsx
+++ src/components/Landing/Brands.tsx
@@ -32,119 +32,119 @@ const Brands = () => {
       <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl space-y-5 font-extrabold text-center text-primary pt-20">
         Trusted by Leading Brands:
       </h1>
-      <div className="w-full max-w-5xl mx-auto px-4 md:px-2 py-20">
+      <div className="w-full px-4 md:px-2 py-20">
         <div className="text-center">
           <div className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)]">
             <ul
               id="logos"
               className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll"
             >
-              <li>
+              <li className="w-48 flex items-center justify-center rounded bg-white h-48 border">
                 <Image
                   src={"/assets/images/grain-client-logos/aldo.svg"}
                   alt="aldo"
                   width={150}
                   height={150}
-                  className="hover:text-gray-900 dark:hover:text-white"
+                  className="hover:text-gray-900 grayscale hover:grayscale-0 transition ease-in-out dark:hover:text-white"
                 />
               </li>
-              <li>
+              <li className="w-48 flex items-center justify-center rounded bg-white h-48 border">
                 <Image
                   src={"/assets/images/grain-client-logos/Bell.svg"}
                   alt="aldo"
                   width={150}
                   height={150}
-                  className="hover:text-gray-900 dark:hover:text-white"
+                  className="grayscale hover:grayscale-0 transition ease-in-out hover:text-gray-900 dark:hover:text-white"
                 />
               </li>
-              <li>
+              <li className="w-48 flex items-center justify-center rounded bg-white h-48 border">
                 <Image
                   src={"/assets/images/grain-client-logos/zayo.svg"}
                   alt="aldo"
                   width={150}
                   height={150}
-                  className="hover:text-gray-900 dark:hover:text-white"
+                  className="grayscale hover:grayscale-0 transition ease-in-out hover:text-gray-900 dark:hover:text-white"
                 />
               </li>
-              <li>
+              <li className="w-48 flex items-center justify-center rounded bg-white h-48 border">
                 <Image
                   src={"/assets/images/grain-client-logos/Brookfield.svg"}
                   alt="aldo"
                   width={150}
                   height={150}
-                  className="hover:text-gray-900 dark:hover:text-white"
+                  className="grayscale hover:grayscale-0 transition ease-in-out hover:text-gray-900 dark:hover:text-white"
                 />
               </li>
-              <li>
+              <li className="w-48 flex items-center justify-center rounded bg-white h-48 border">
                 <Image
                   src={"/assets/images/grain-client-logos/canadian-tire.svg"}
                   alt="aldo"
                   width={150}
                   height={150}
-                  className="hover:text-gray-900 dark:hover:text-white"
+                  className="grayscale hover:grayscale-0 transition ease-in-out hover:text-gray-900 dark:hover:text-white"
                 />
               </li>
-              <li>
+              <li className="w-48 flex items-center justify-center rounded bg-white h-48 border">
                 <Image
                   src={"/assets/images/grain-client-logos/Deloitte.svg"}
                   alt="aldo"
                   width={150}
                   height={150}
-                  className="hover:text-gray-900 dark:hover:text-white"
+                  className="grayscale hover:grayscale-0 transition ease-in-out hover:text-gray-900 dark:hover:text-white"
                 />
               </li>
-              <li>
+              <li className="w-48 flex items-center justify-center rounded bg-white h-48 border">
                 <Image
                   src={"/assets/images/grain-client-logos/homesquare.svg"}
                   alt="aldo"
                   width={150}
                   height={150}
-                  className="hover:text-gray-900 dark:hover:text-white"
+                  className="grayscale hover:grayscale-0 transition ease-in-out hover:text-gray-900 dark:hover:text-white"
                 />
               </li>
-              <li>
+              <li className="w-48 flex items-center justify-center rounded bg-white h-48 border">
                 <Image
                   src={"/assets/images/grain-client-logos/Queens.svg"}
                   alt="aldo"
                   width={150}
                   height={150}
-                  className="hover:text-gray-900 bg-black p-2 dark:hover:text-white"
+                  className="hover:text-gray-900 grayscale hover:grayscale-0 transition ease-in-out bg-black p-2 dark:hover:text-white"
                 />
               </li>
-              <li>
+              <li className="w-48 flex items-center justify-center rounded bg-white h-48 border">
                 <Image
                   src={"/assets/images/grain-client-logos/sportchek.svg"}
                   alt="aldo"
                   width={150}
                   height={150}
-                  className="hover:text-gray-900 bg-black p-2 dark:hover:text-white"
+                  className="hover:text-gray-900 grayscale hover:grayscale-0 transition ease-in-out bg-black p-2 dark:hover:text-white"
                 />
               </li>
-              <li>
+              <li className="w-48 flex items-center justify-center rounded bg-white h-48 border">
                 <Image
                   src={"/assets/images/grain-client-logos/tdbank.png"}
                   alt="aldo"
                   width={150}
                   height={150}
-                  className="hover:text-gray-900 dark:hover:text-white"
+                  className="grayscale hover:grayscale-0 transition ease-in-out  hover:text-gray-900 dark:hover:text-white"
                 />
               </li>
-              <li>
+              <li className="w-48 flex items-center justify-center rounded bg-white h-48 border">
                 <Image
                   src={"/assets/images/grain-client-logos/UniToronto.svg"}
                   alt="aldo"
                   width={150}
                   height={150}
-                  className="hover:text-gray-900 bg-black p-2 dark:hover:text-white"
+                  className="hover:text-gray-900 grayscale hover:grayscale-0 transition ease-in-out bg-black p-2 dark:hover:text-white"
                 />
               </li>
-              <li>
+              <li className="w-48 flex items-center justify-center rounded bg-white h-48 border">
                 <Image
                   src={"/assets/images/grain-client-logos/vale.svg"}
                   alt="aldo"
                   width={150}
                   height={150}
-                  className="hover:text-gray-900 dark:hover:text-white"
+                  className="grayscale hover:grayscale-0 transition ease-in-out hover:text-gray-900 dark:hover:text-white"
                 />
               </li>
             </ul>
