51971429666753a8f58a8ddf0a0d2d633b46b7cc
Harsh Kumar Raghav <raghavharsh068@gmail.com>
2023-10-19 23:57:05 +0530

    Updated Flip on button click and companies scroll

--- src/components/Landing/CaseStudy.tsx
+++ src/components/Landing/CaseStudy.tsx
@@ -1,11 +1,23 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { Card, CardContent, CardDescription, CardTitle } from "../ui/card";
 import { Casetudy } from "@/utils/Data/Casetudy";
 import Image from "next/image";
-import { Plus } from "lucide-react";
 import { Button } from "../ui/button";
+import { Plus } from "lucide-react";
 
 const CaseStudy = () => {
+  const [flippedCardId, setFlippedCardId] = useState(null);
+
+  const handleCardClick = (cardId: any) => {
+    if (flippedCardId === cardId) {
+      // If the clicked card is already flipped, unflip it
+      setFlippedCardId(null);
+    } else {
+      // Flip the clicked card
+      setFlippedCardId(cardId);
+    }
+  };
   return (
     <div className="grid grid-cols-1 md:grid-cols-3">
       {Casetudy.map((item) => (
@@ -23,7 +35,11 @@ const CaseStudy = () => {
             </>
           ) : (
             <Card className="w-full h-96 bg-transparent cursor-pointer group perspective">
-              <div className="relative preserve-3d group-hover:my-rotate-y-180 w-full h-full duration-1000">
+              <div
+                className={`relative preserve-3d  w-full h-full duration-1000 ${
+                  flippedCardId === item.id ? "my-rotate-y-180" : ""
+                }`}
+              >
                 <div className="absolute backface-hidden border-2 flex flex-col items-center gap-8 justify-center w-full h-full">
                   <Image
                     src={item.image}
@@ -39,16 +55,22 @@ const CaseStudy = () => {
                   <CardDescription className="text-center">
                     {item.desc}
                   </CardDescription>
+                  <div
+                    className="rounded-full cursor-pointer bg-primary w-10 h-10 text-white flex justify-center items-center"
+                    onClick={() => handleCardClick(item.id)}
+                  >
+                    <Plus className="w-4 h-4" />
+                  </div>
                 </div>
-                <CardContent className="absolute my-rotate-y-180 backface-hidden w-full h-full bg-secondary overflow-hidden">
+                <CardContent
+                  className="absolute my-rotate-y-180 backface-hidden w-full h-full bg-secondary overflow-hidden"
+                  onClick={() => handleCardClick(item.id)}
+                >
                   <div className="text-center flex flex-col items-center py-4 md:py-14 h-full text-gray-800 px-2 md:gap-4 gap-1 ">
                     <h1 className="text-xl md:text-3xl text-primary font-semibold">
                       {item.title}
                     </h1>
                     <p className="text-sm text-gray-600">{item.content}</p>
-                    <button className="bg-primary rounded-lg px-6 py-2 font-semibold text-white  absolute -bottom-20 delay-500 duration-1000 group-hover:bottom-10 scale-0 group-hover:scale-125">
-                      Learn More
-                    </button>
                   </div>
                 </CardContent>
               </div>
