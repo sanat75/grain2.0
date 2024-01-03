9554f8d55a94a2eefc3e6d81b7f28beb8c3f5ae0
HarshKumarraghav <raghavharsh068@gmail.com>
2024-01-03 10:24:58 +0530

    uplaoded video used

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -1,15 +1,17 @@
 import Footer from "@/components/Footer/Footer";
 import Home from "@/components/Home/Home";
 import Header from "@/components/Navigation/Header";
+import type { Metadata } from "next";
 import Head from "next/head";
+export const metadata: Metadata = {
+  title: "Grainanalytics",
+  description:
+    "Grain Analytics is a data and analytics consulting firm that helps organizations use data to make better decisions. We work with companies to build data-driven cultures, develop data strategies, and create data products.",
+};
 
 const LandingLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <Head>
-        <title>Grainanalytics</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
       <div className="relative w-screen h-[600px] md:h-screen overflow-hidden">
         <Header />
         {children}
