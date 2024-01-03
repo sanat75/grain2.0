3ca88adb1987648f81ac62c76c1e44e48c4ac92a
HarshKumarraghav <raghavharsh068@gmail.com>
2024-01-03 10:21:28 +0530

    uplaoded video used

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -1,10 +1,15 @@
 import Footer from "@/components/Footer/Footer";
 import Home from "@/components/Home/Home";
 import Header from "@/components/Navigation/Header";
+import Head from "next/head";
 
 const LandingLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
+      <Head>
+        <title>Grainanalytics</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <div className="relative w-screen h-[600px] md:h-screen overflow-hidden">
         <Header />
         {children}
