8a28147b6567731af4992555f794bd71ae78a681
HarshKumarraghav <raghavharsh068@gmail.com>
2024-01-06 18:40:50 +0530

    spell correction

--- src/app/services/customeranalytics/page.tsx
+++ src/app/services/customeranalytics/page.tsx
@@ -55,7 +55,7 @@ const page = () => {
       </Wrapper>
       <div className=" md:p-10 py-10 px-3 text-center  ">
         <h1 className="text-xl font-extrabold  text-primary uppercase">
-          Supporting the largest tech and data platforms
+          Supporting largest tech and data platforms
         </h1>
         {/* <p className="text-sm text-gray-600">
             Our approach starts with understanding your data evolution stage.
