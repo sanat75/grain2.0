cd8bd1a7264388143e48088d7e9fbdcf7b4bce81
Sanat <sanat23122003@gmail.com>
2025-03-12 16:54:47 +0530

    Fix: Set Next.js standalone output

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ["i.ibb.co", "utfs.io"],
-  },
+  output: 'standalone'
 };
 
 module.exports = nextConfig;
